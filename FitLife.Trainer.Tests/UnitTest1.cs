a5f83545a30f64cd19071c1762b184d62c0a7d5e
Rasmus Møller Kristensen <eaa24ramk@students.eaaa.dk>
2026-05-07 10:36:07 +0200

    Initial project structure

---
+++ FitLife.Trainer.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace FitLife.Trainer.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
